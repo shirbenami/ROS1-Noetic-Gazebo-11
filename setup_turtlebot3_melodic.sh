1e05d6a9c89cf8562264f47e1805afbeb222f9c9
Shir Ben Ami <80039457+shirbenami@users.noreply.github.com>
2025-05-27 17:33:41 +0300

    Update setup_turtlebot3_melodic.sh

--- setup_turtlebot3_melodic.sh
+++ setup_turtlebot3_melodic.sh
@@ -1,22 +1,26 @@
+#!/bin/bash
+
+  GNU nano 2.9.3  setup_turtlebot3_melodic.sh             
+
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 
-git clone -b noetic-devel https://github.com/R$
-git clone -b noetic-devel https://github.com/R$
+git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
+git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
 
 cd ~/catkin_ws
 rosdep update
-rosdep install --from-paths src --ignore-src -$
+rosdep install --from-paths src --ignore-src -r -y
 
 catkin_make
 
-echo "export TURTLEBOT3_MODEL=waffle" >> ~/.ba$
+echo "export TURTLEBOT3_MODEL=waffle" >> ~/.bashrc
 export TURTLEBOT3_MODEL=waffle
 
 source /opt/ros/melodic/setup.bash
 source devel/setup.bash
 
-roslaunch turtlebot3_gazebo turtlebot3_world.l$
+roslaunch turtlebot3_gazebo turtlebot3_world.launch
 
 
 
